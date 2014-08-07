25647607fe1bc68170e727847c80845c58624e95
Philippe Faist <pfaist@ethz.ch>
2014-08-07 14:01:18 +0200

    added vacuum filter

---
+++ vacuum.py
@@ -0,0 +1,67 @@
+
+# Filter to remove all entries from the database.
+
+from pybtex.database import BibliographyData, Entry;
+
+from core.bibfilter import BibFilter, BibFilterError;
+from core.blogger import logger;
+from core.pylatexenc import latex2text
+
+
+
+# --------------------------------------------------
+
+HELP_AUTHOR = u"""\
+vacuum filter by Philippe Faist, ETHZ, (C) 2014, GPL 3+
+"""
+
+HELP_DESC = u"""\
+Filter that removes all entries from the database. Only useful in conjunction with and
+after, e.g., bib2enxml filter.
+"""
+
+HELP_TEXT = u"""
+Very simple. Leaves no database entry behind. Saves space in the bibolamazi file. Very
+clean.
+"""
+
+
+class VacuumFilter(BibFilter):
+
+    helpauthor = HELP_AUTHOR
+    helpdescription = HELP_DESC
+    helptext = HELP_TEXT
+
+    def __init__(self):
+        """
+        Vacuum filter constructor.
+        """
+
+        BibFilter.__init__(self);
+
+
+    def name(self):
+        return "vacuum"
+
+    def getRunningMessage(self):
+        return u"Vacuuming all entries"
+
+    def action(self):
+        return BibFilter.BIB_FILTER_BIBOLAMAZIFILE;
+
+
+    def filter_bibolamazifile(self, bibolamazifile):
+        #
+        # bibdata is a pybtex.database.BibliographyData object
+        #
+
+        bibdata = BibliographyData()
+
+        bibolamazifile.setBibliographyData(bibdata)
+
+        return
+
+
+def bibolamazi_filter_class():
+    return VacuumFilter;
+
